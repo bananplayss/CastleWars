cabbc084fe325bdcbda1ecba6099ef5825efbea1
bananplaysshu <borokbence8@gmail.com>
2026-06-15 23:20:16 +0200

    Random code snippetek

---
+++ core/src/main/java/me/bananplayss/castlewars/core/utils/Vectors/Vector3i.java
@@ -0,0 +1,41 @@
+package me.bananplayss.castlewars.core.utils.Vectors;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.bukkit.Bukkit;
+
+@Getter @Setter
+public class Vector3i {
+    private int x,y,z;
+
+    public Vector3i(int x, int y, int z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public Vector3i add(int x, int y, int z){
+        this.y += y;
+        this.x += x;
+        this.z += z;
+
+        return this;
+    }
+
+    public Vector3i multiply(int multiplier){
+        this.x *= multiplier;
+        this.y *= multiplier;
+        this.z *= multiplier;
+
+        return this;
+    }
+
+    public static Vector3i fromString(String string){
+        String[] args = string.split(",");
+        if(args.length != 3) {
+            Bukkit.getLogger().severe("Cannot parse " + string + " to Vector3i");
+            return null;
+        }
+        return new Vector3i(Integer.parseInt(args[0]),Integer.parseInt(args[1]),Integer.parseInt(args[2]));
+    }
+}
